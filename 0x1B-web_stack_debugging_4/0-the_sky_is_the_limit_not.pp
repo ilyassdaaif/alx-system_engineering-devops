ec7fe6def4f799e3c884fe5323e4c279294b0c68
ilyassdf <ilyass>
2024-06-13 16:02:20 +0000

    origin

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/worker_connections 768;/worker_connections 4096;/g" /etc/nginx/nginx.conf && nginx -s reload',
-  onlyif  => 'grep -q "worker_connections 768;" /etc/nginx/nginx.conf',
+  command => 'sed -i "s/worker_processes [0-9]*/worker_processes 4/;s/worker_connections [0-9]*/worker_connections 1024/;s/keepalive_timeout [0-9]*/keepalive_timeout 65/" /etc/nginx/nginx.conf && service nginx restart',
+  path    => ['/bin', '/usr/bin'],
 }
