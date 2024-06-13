96031a4276a38ae01028288463811379bf32a56a
ilyass <ilyassdf@gmail.com>
2024-06-13 13:15:37 -0700

    origin

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,9 +5,10 @@ exec { 'fix--for-nginx':
   command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/',
 }
-#->
+
 # Restart Nginx
 exec { 'nginx-restart':
   command => '/etc/init.d/nginx restart',
   path    => '/etc/init.d/',
+  require => Exec['fix--for-nginx'],
 }
